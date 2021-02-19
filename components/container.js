bc53410a338924b00efd762c719fd88be1e1b936
Alan <49978973+3Alan@users.noreply.github.com>
2021-02-19 14:25:32 +0800

    Delete container.js

--- components/container.js
+++
@@ -1,7 +0,0 @@
-export default function Container({ children, width = 'max-w-screen-lg', bgColor = 'bg-gray-50' }) {
-    return (
-        <div className={`w-full pb-10 dark:bg-dark-bg transition-colors ${bgColor}`}>
-            <div className={`container mx-auto ${width}`}>{children}</div>
-        </div>
-    );
-}
