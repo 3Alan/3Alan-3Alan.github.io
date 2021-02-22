0586fa2d427483b9f9cc6d8a16aac3732ac90f8a
王平 <1540703192@qq.com>
2021-02-22 14:19:14 +0800

    优化导航栏，更新tailwindcss

--- components/navigator/index.js
+++ components/navigator/index.js
@@ -27,7 +27,7 @@ function HomeIcon() {
 
 function NavItem({ name, link = '/' }) {
     return (
-        <div className="px-4 text-sm font-mono text-gray-600 dark:text-gray-300">
+        <div className="px-4 font-semibold text-sm font-mono hover:text-blue-500  text-gray-500 dark:text-gray-300 dark:hover:text-blue-400">
             <Link href={link}>
                 <a>{name}</a>
             </Link>
@@ -50,35 +50,25 @@ function NavList() {
 export default function Navigator() {
     let scroll;
     const router = useRouter();
-    const [opacityvalue, setOpacityvalue] = useState(1);
     const [isFullPage, setIsFullPage] = useState(false);
 
     if (CSR) {
-        scroll = useScroll(document, (val) => val.top < 300);
+        scroll = useScroll(document);
     }
 
     useEffect(() => {
         if (FULL_PAGE_LIST.includes(router.pathname)) {
-            setOpacityvalue(0);
+            console.log('hashshdfhds', isFullPage);
             setIsFullPage(true);
         }
     }, [router]);
 
-    useEffect(() => {
-        if (isFullPage) {
-            setOpacityvalue(Math.floor(scroll.top / 20) * 10);
-            console.log(Math.floor(scroll.top / 20) * 10);
-        }
-    }, [scroll]);
-
     return (
         <div
-            className={cn(
-                `fixed sm:flex bg-white hidden h-16 inset-0 overflow-hidden z-50 transition-colors bg-opacity-${opacityvalue} dark:bg-dark-content`,
-                {
-                    'shadow-navigator': !isFullPage || opacityvalue > 50
-                }
-            )}
+            className={cn(`fixed transition-all bg-white hidden h-16 inset-0 z-50 dark:bg-dark-content`, {
+                'shadow-navigator': !isFullPage || (scroll && scroll.top > 200),
+                'sm:flex': !isFullPage || (scroll && scroll.top > 200)
+            })}
         >
             <div className="max-w-screen-lg mx-auto flex w-full justify-between items-center py-2">
                 <HomeIcon />
